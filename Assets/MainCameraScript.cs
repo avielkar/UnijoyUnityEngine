0dfd68fcf6b51d9455557be5c12d84f89e339737
avielkar <avraham.elkar@gmail.com>
2020-04-03 15:15:46 +0300

    Add simple red triangle with camera and script demo train

---
+++ Assets/MainCameraScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraScript : MonoBehaviour
+{
+    private float y = 0;
+    private float z = 0;
+    private float x = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.LookAt(new Vector3(0, 0, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x += 0.1f;
+        //transform.LookAt(new Vector3(-x, 0, 0));
+        transform.position = new Vector3(0, 0, -x);
+    }
+}
