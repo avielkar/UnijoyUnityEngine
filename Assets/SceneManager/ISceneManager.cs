dda4fa371f7a02c41d4d5a279ae453a36a9f51af
avielkar <avraham.elkar@gmail.com>
2020-04-04 14:55:32 +0300

    Adding SceneManager basics

---
+++ Assets/SceneManager/ISceneManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Assets.SceneBuilders;
+
+namespace Assets.SceneManager
+{
+    public interface ISceneManager<T> where T : ISceneData
+    {
+
+    }
+}
---
+++ Assets/SceneManager/SceneManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Assets.Data;
+using Assets.Network.Handlers;
+using Assets.Network.Retrievers;
+using Assets.SceneBuilders;
+
+namespace Assets.SceneManager
+{
+    public class SceneManager<T,M>:ISceneManager<T> where T : ISceneData where M:ITrialData
+    {
+        private ISceneBuilder<T> _sceneBuilder;
+        private IDataRetriever<M> _dataRetriever;
+        private IDataHandler<M> _dataHandler;
+
+        public SceneManager(
+            ISceneBuilder<T> sceneBuilder,
+            IDataRetriever<M> dataRetriever,
+            IDataHandler<M> dataHandler)
+        {
+            _sceneBuilder = sceneBuilder;
+            _dataRetriever = dataRetriever;
+            _dataHandler = dataHandler;
+        }
+    }
+}
