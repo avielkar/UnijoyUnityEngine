82f30445a4dd884689b1090454e4384c27929fb8
avielkar <avraham.elkar@gmail.com>
2020-04-04 03:13:46 +0300

    Adding data handlers logic

---
+++ Assets/Network/Handlers/CommandsHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Network.Handlers
+{
+    public class CommandsHandler : IDataHandler
+    {
+        public void Handle(string data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Network/Handlers/IDataHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Network.Handlers
+{
+    public interface IDataHandler
+    {
+        void Handle(string data);
+    }
+}
