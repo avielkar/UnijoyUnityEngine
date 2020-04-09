88ef238a50221f2e804d681610cde11f67766228
avielkar <avraham.elkar@gmail.com>
2020-04-10 00:09:12 +0300

    Adding logs when getting commands

--- Assets/Network/Handlers/CommandsRetriever.cs
+++ Assets/Network/Handlers/CommandsRetriever.cs
@@ -58,6 +58,7 @@ namespace Assets.Network.Handlers
             void _server_DelimiterDataReceived(object sender, Message e)
             {
                 var commandValuePair = e.MessageString.Split(_commandSplitChar);
+                Debug.Log($"Received new command {commandValuePair[0]} with value {commandValuePair[1]}");
                 _commandsBuffer.Enqueue((commandValuePair[0], commandValuePair[1]));
             }
         }
